5e97ef01a5923eb586907b65c0c118e4781ece63
Sourabh Kumar <20010001030@gateway.edu.in>
2022-07-31 20:50:57 +0530

    Added custom back navigation

--- app/src/main/java/com/example/android/sports/SportsListFragment.kt
+++ app/src/main/java/com/example/android/sports/SportsListFragment.kt
@@ -20,9 +20,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.navigation.fragment.findNavController
+import androidx.slidingpanelayout.widget.SlidingPaneLayout
 import com.example.android.sports.databinding.FragmentSportsListBinding
 
 /**
@@ -46,6 +47,8 @@ class SportsListFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val binding = FragmentSportsListBinding.bind(view)
 
+
+
         // Initialize the adapter and set it to the RecyclerView.
         val adapter = SportsAdapter {
             // Update the user selected sport as the current sport in the shared viewmodel
@@ -54,9 +57,35 @@ class SportsListFragment : Fragment() {
             // Navigate to the details screen
 //            val action = SportsListFragmentDirections.actionSportsListFragmentToNewsFragment()
 //            this.findNavController().navigate(action)
-              binding.slidingPaneLayout.openPane()
+            binding.slidingPaneLayout.openPane()
         }
         binding.recyclerView.adapter = adapter
         adapter.submitList(sportsViewModel.sportsData)
+    }
+}
+
+//custom back navigation
+class SportsListOnBackPressedCallback(
+    private val slidingPaneLayout: SlidingPaneLayout
+) : OnBackPressedCallback(slidingPaneLayout.isSlideable && slidingPaneLayout.isOpen),
+    SlidingPaneLayout.PanelSlideListener {
+
+    init {
+        slidingPaneLayout.addPanelSlideListener(this)
+    }
+    override fun handleOnBackPressed() {
+        slidingPaneLayout.closePane()
+    }
+
+    override fun onPanelSlide(panel: View, slideOffset: Float) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onPanelOpened(panel: View) {
+        isEnabled(true)
+    }
+
+    override fun onPanelClosed(panel: View) {
+        isEnabled(true)
     }
 }
